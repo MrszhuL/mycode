e375ca671bfc6df0feaa73d49f7dddbc169a775a
MrszhuL <2837815584@qq.com>
2020-11-23 21:24:49 +0800

    first commit

---
+++ gitt/gitt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitt
+//
+//  Created by mac on 2020/11/23.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
